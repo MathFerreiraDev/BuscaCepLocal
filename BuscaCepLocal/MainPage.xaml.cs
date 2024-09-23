9feb50f8ed3de6a2b171e0ebaf37b669a13c9ba0
Usuario <Usuario@CPD03-M18>
2024-09-23 12:19:49 -0300

    Atualização da API de obtenção de dados :)

--- BuscaCepLocal/MainPage.xaml.cs
+++ BuscaCepLocal/MainPage.xaml.cs
@@ -1,25 +1,33 @@
-﻿namespace BuscaCepLocal
+﻿using BuscaCepLocal.Model;
+using BuscaCepLocal.Services;
+
+namespace BuscaCepLocal
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+       
+
+        private async  void   Button_Clicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            try
+            {
+                Endereco? dados_endereco = await DataServiceCep.GetEnderecoByCep(txt_cep.Text);
+                BindingContext = dados_endereco;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
+
+        
     }
 
 }
--- BuscaCepLocal/Services/DataServiceCep.cs
+++ BuscaCepLocal/Services/DataServiceCep.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using BuscaCepLocal.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
 
 namespace BuscaCepLocal.Services
 {
     public class DataServiceCep
     {
+        public static async Task<Endereco?> GetEnderecoByCep(string cep)
+        {
+            Endereco? end;
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(
+                    "http://localhost:8000/endereco/by-cep?cep=" + cep);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = response.Content.ReadAsStringAsync().Result;
+                    end = JsonConvert.DeserializeObject<Endereco?>(json);
+                }
+                else
+                {
+                    throw new Exception(response.RequestMessage.Content.ToString());
+                }
+
+                
+
+            }
+
+            return end;
+
+        }
     }
 }
